4450829387164a94282ee51eff34f639cff7ca26
Juan Pablo <jkalemke@gmail.com>
2023-04-26 15:36:50 -0300

    first commit

---
+++ src/TestReferencia.java
@@ -0,0 +1,26 @@
+import java.awt.dnd.peer.DragSourceContextPeer;
+
+import javax.sql.rowset.serial.SQLOutputImpl;
+
+public class TestReferencia {
+	
+	public static void main(String[] args) {
+		Cuenta primeraCuenta = new Cuenta();
+		primeraCuenta.saldo = 200;
+		
+		Cuenta segundaCuenta = primeraCuenta;
+		segundaCuenta.saldo = 100;
+		
+		System.out.println("Saldo primera" 
+		         + "Cuenta: " + primeraCuenta.saldo);
+		
+		System.out.println("Saldo segunda" 
+		         + "Cuenta: " + segundaCuenta.saldo);
+		segundaCuenta.saldo +=400;
+		
+		System.out.println("Saldo primera" 
+		         + "Cuenta: " + primeraCuenta.saldo);
+		
+	}
+
+}
